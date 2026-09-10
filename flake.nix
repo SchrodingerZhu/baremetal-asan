760d3ba03f218b85479dfe8f1153900a93477dcb
Schrodinger ZHU Yifan <i@zhuyi.fan>
2026-09-10 12:37:01 -0400

    [BaremetalAsan] Package LLVM tools and RA8M2 runtime with Nix

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "LLVM 23/libc++ development environment for bare-metal ASan on RA8xx";
+  description = "Patched LLVM tools and bare-metal ASan runtime for RA8xx";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -21,6 +21,84 @@
       rustToolchain = pkgs.rust-bin.stable.latest.default.override {
         targets = [ "thumbv8m.main-none-eabihf" ];
         extensions = [ "rust-src" ];
+      };
+
+      rustPlatform = pkgs.makeRustPlatform {
+        cargo = rustToolchain;
+        rustc = rustToolchain;
+      };
+
+      # Local path inputs can include bytecode left by previous LLVM tests.
+      sourceFiles = paths: pkgs.lib.fileset.unions (map
+        (path: pkgs.lib.fileset.fileFilter
+          (file: !(file.hasExt "pyc" || file.hasExt "pyo")) path)
+        paths);
+
+      asanLLVM = llvm.libcxxStdenv.mkDerivation {
+        pname = "baremetal-asan-llvm";
+        version = "24.0.0-dev";
+        src = pkgs.lib.fileset.toSource {
+          root = ./.;
+          fileset = sourceFiles [
+            ./llvm ./clang ./lld ./cmake ./third-party ./libc ./libunwind
+          ];
+        };
+        cmakeDir = "../llvm";
+        nativeBuildInputs = [ pkgs.cmake pkgs.ninja pkgs.python3 pkgs.perl ];
+        buildInputs = [ pkgs.zlib pkgs.zstd ];
+        cmakeFlags = [
+          "-DCMAKE_BUILD_TYPE=Release"
+          "-DLLVM_ENABLE_PROJECTS=clang;lld"
+          "-DLLVM_TARGETS_TO_BUILD=Native;ARM"
+          "-DLLVM_ENABLE_LIBCXX=ON"
+          "-DLLVM_USE_LINKER=${llvm.bintools}/bin/ld.lld"
+          "-DLLVM_ENABLE_ASSERTIONS=ON"
+          "-DLLVM_APPEND_VC_REV=OFF"
+          "-DLLVM_INCLUDE_TESTS=OFF"
+          "-DLLVM_INCLUDE_BENCHMARKS=OFF"
+          "-DLLVM_INCLUDE_EXAMPLES=OFF"
+          "-DLLVM_ENABLE_BINDINGS=OFF"
+          "-DLLVM_ENABLE_LIBEDIT=OFF"
+          "-DLLVM_ENABLE_LIBXML2=OFF"
+          "-DLLVM_DISTRIBUTION_COMPONENTS=clang;clang-resource-headers;lld;llvm-ar;llvm-ranlib;llvm-nm;llvm-objdump;llvm-objcopy;llvm-readobj;llvm-readelf;llvm-size"
+        ];
+        enableParallelBuilding = true;
+        ninjaFlags = [ "distribution" ];
+        installTargets = [ "install-distribution" ];
+      };
+
+      asanRuntime = pkgs.stdenvNoCC.mkDerivation {
+        pname = "ra8x2-asan-rt";
+        version = "0.1.0";
+        src = pkgs.lib.fileset.toSource {
+          root = ./.;
+          fileset = sourceFiles [
+            ./Cargo.toml ./Cargo.lock ./baremetal-asan-rt ./ra8x2-asan-rt ./libc
+          ];
+        };
+        cargoDeps = rustPlatform.importCargoLock { lockFile = ./Cargo.lock; };
+        nativeBuildInputs = [
+          rustToolchain rustPlatform.cargoSetupHook llvm.clang-unwrapped llvm.llvm
+        ];
+        CXX = "${llvm.clang-unwrapped}/bin/clang++";
+        AR = "${llvm.llvm}/bin/llvm-ar";
+        CXX_thumbv8m_main_none_eabihf = "${llvm.clang-unwrapped}/bin/clang++";
+        AR_thumbv8m_main_none_eabihf = "${llvm.llvm}/bin/llvm-ar";
+        dontConfigure = true;
+        dontStrip = true;
+        buildPhase = ''
+          runHook preBuild
+          cargo rustc --offline --locked -p ra8x2-asan-rt --release \
+            --no-default-features --target thumbv8m.main-none-eabihf \
+            -- -C target-cpu=cortex-m85
+          runHook postBuild
+        '';
+        installPhase = ''
+          runHook preInstall
+          mkdir -p "$out/lib"
+          cp target/thumbv8m.main-none-eabihf/release/libra8x2_asan_rt.a "$out/lib/"
+          runHook postInstall
+        '';
       };
 
       python = pkgs.python3.withPackages (ps: with ps; [
@@ -46,6 +124,12 @@
           "$@"
       '';
     in {
+      packages.${system} = {
+        llvm = asanLLVM;
+        ra8x2-asan-rt = asanRuntime;
+        default = asanLLVM;
+      };
+
       devShells.${system}.default = (pkgs.mkShell.override {
         stdenv = llvm.libcxxStdenv;
       }) {
